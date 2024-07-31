da6d05eb63b52736a79d82586dcdd2ed777ff021
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2024-07-31 17:46:43 +0530

    Add files via upload

---
+++ Multiple.java
@@ -0,0 +1,28 @@
+package startproject;
+
+import java.util.Scanner;
+
+public class Multiple 
+{
+	public static void main(String[] args)
+	{
+		
+	
+	Scanner  scan= new Scanner(System.in);
+	int n=scan.nextInt();
+	if(n%10==0)
+	{
+		System.out.print(" YES");
+	
+	}
+	else
+	{
+		System.out.print("No");
+	}
+	
+	
+	
+	}
+	
+
+}
