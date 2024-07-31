83387c41fd56ccea6b07e6f71674c79804ee141b
SOWMIYA0204 <135124221+SOWMIYA0204@users.noreply.github.com>
2024-07-31 17:43:41 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,117 @@
+package startproject;
+
+public class strings {
+  public static void main(String [] args)
+  {
+//	  //without using new
+//	  String fs="Software";
+//	  String ss="Software";
+//	  if(fs==ss)
+//	  {
+//		  System.out.println("ref are equal because duplicates are not allowed in Constant pool");
+//	  }
+//	  else
+//	  {
+//		  System.out.println("ref are not equal");
+//	  }
+//	  String s1=new String("SOWMIYA");
+//	  String s2=new String("sowmiy");
+//	  //double equal to(==) method
+//	  if(s1==s2)
+//	  {
+//		  System.out.println("Ref are equal");
+//	  
+//	  else
+//	  {
+//		  System.out.println("Ref are not equal");
+//	  }
+//	  //equals() method
+//	  if(s1.equals(s2))
+//	  {
+//		  System.out.println("Values are equal");
+//	  }
+//	  else
+//	  {
+//		  System.out.println("Values are not equal");
+//	  }
+//	  //equalsIgnoreCase method
+//      if(s1.equalsIgnoreCase(s2))
+//      {
+//    	  System.out.println("Value are Same but Case different");
+//      }
+//      else
+//      {
+//    	  System.out.println("Values are not same");
+//      }
+//  }
+	  //Strings concat examples
+//	  String s1="SOWMIYA";
+//	  String s2="SOFTWAREENGINEER";
+//	  String s3="SOWMIYA"+"SOFTWAREENGINEER";
+//	  String s4="SOWMIYA"+"SOFTWAREENGINEER";
+//	  String s5=s1.concat(s2);
+//	  String s6=s1.concat(s2);
+//	  if(s3==s4)
+//	  {
+//		  System.out.println("ref are equal");
+//	  }
+//	  else
+//	  {
+//		  System.out.println("Ref are not equal");
+//	  }
+//	  if(s5.equals(s6))
+//	  {
+//		  System.out.println("Memory created in non constant pool");
+//	  }
+//	  else
+//	  {
+//		  System.out.println("Values are not equal");
+//	  }
+	  
+	  //In-built methods in java
+	  String str="SowmiyaSoftware";
+	  System.out.println(str.toUpperCase());	
+	  System.out.println(str.toLowerCase());
+	  System.out.println(str.length());
+	  System.out.println(str.indexOf('S'));
+	  System.out.println(str.charAt(6));
+	  System.out.println(str.lastIndexOf('S'));
+	  System.out.println(str.startsWith("sowm"));
+	  System.out.println(str.startsWith("Sowmi"));
+	  System.out.println(str.contains("miya"));
+	  System.out.println(str.substring(2,6));
+	
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+	  
+}
+}
+  
